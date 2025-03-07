51fc3013f75c596e237be9000dc3fd4567a5f04a
Kusmar00 <markusraunjohansen@gmail.com>
2025-03-07 17:11:52 +0100

    add resp.go to deserialize client messages

--- app/main.go
+++ app/main.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Listening on port :6379")
+
+	// create a new server
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+
+	// listen for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting:", err)
+		return
+	}
+
+	defer conn.Close() // close connection once finished
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(value)
+
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KusMar00/redis-in-go
+
+go 1.23
