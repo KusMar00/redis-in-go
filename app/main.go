da215bfbdad35ce04f9be9800e94679ba487d0ae
Kusmar00 <markusraunjohansen@gmail.com>
2025-03-11 17:43:37 +0100

    Add data persisting with AOF

--- app/main.go
+++ app/main.go
@@ -17,6 +17,27 @@ func main() {
 		return
 	}
 
+	// load AOF
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
+
 	// listen for connections
 	conn, err := l.Accept()
 	if err != nil {
@@ -53,6 +74,10 @@ func main() {
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)
